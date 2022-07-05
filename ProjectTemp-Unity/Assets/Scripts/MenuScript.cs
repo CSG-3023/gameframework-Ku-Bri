55e78b04711e45e2f625dde418f3d710d62e3db0
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2022-07-05 17:42:43 -0500

    Created MenuScript and assigned textboxes and buttons
    Created generic script to be used for both MainMenus and EndScene. Linked textboxes, and linked buttons to appropriate functions. Tested for Debug.Log and is working as expected.

---
+++ ProjectTemp-Unity/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+/****
+ * Created by: Bridget Kurr
+ * Date Created: June 5, 2022
+ * 
+ * Last Edited by:
+ * Last Edit:
+ * 
+ * Description: Manages the UI elements for the menu scenes
+ * ***/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI; //libraries for UI components
+using TMPro; //libraries for TextMeshPro componenets
+
+public class MenuScript : MonoBehaviour
+{
+
+    [Header("Text Boxes")]
+    public TMP_Text titleTextbox; //textbox for the title
+    public TMP_Text creditsTextbox; //textbox for the credits
+    public TMP_Text copyrightTextbox; //textbox for the copyright
+    public TMP_Text messageTextbox; //textbox for end message
+    public TMP_Text message2Textbox; //textbox for secondary end message
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the value for the textboxes
+    }
+
+    public void OnGameStart()
+    {
+        Debug.Log("Game Started");
+    } //end OnGameStart()
+
+    public void OnGameExit()
+    {
+        Debug.Log("Exited Game");
+    } //end OnGameExit()
+
+  
+} //end MenuScript class
